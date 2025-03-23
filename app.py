a23278848ed52c9f5ccb23c9d9e9b4ead0a0b551
jacoarro99 <jacoarro@gmail.com>
2025-03-23 09:41:09 +0000

    Update app.py

--- app.py
+++ app.py
@@ -322,4 +322,4 @@ def display_error_metrics(data, selected_metrics):
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(host='0.0.0.0', port=int(os.environ.get('PORT', 8050)))
